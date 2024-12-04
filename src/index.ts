79669a7890ac93362389a1e52fa68bf446d50266
Hanif Bahari <hanif.bahari@gmail.com>
2024-12-04 12:10:45 +0100

    Fixed misssing exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { fuzzySearch } from './fuzzySearch';
+export { fuzzySearch, rankedFuzzySearch, prefixSearch, suffixSearch, wildcardSearch } from './fuzzySearch';
 export { fullTextSearch } from './fullTextSearch';
 export { applyFilters } from './filters';
 export { sortData } from './sort';
