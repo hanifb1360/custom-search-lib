749b3be65fa6939987306e6e1deb2959ca963ff2
Hanif Bahari <hanif.bahari@gmail.com>
2024-12-07 23:31:33 +0100

    fixed missed exports in the latest release

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { wildcardSearch } from './search/wildcardSearch';
 export { prefixSearch, suffixSearch } from './search/prefixSuffixSearch';
 export { optimizedLevenshtein } from './utils/levenshtein';
 export { normalizeText } from './utils/textUtils';
+export { applyFilters } from './filters';
+export { sortData } from './sort';
+export { generateFacets } from './analysis'; 
